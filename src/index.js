5d53f8f9f43744372ea0925983eb7aae525ea96e
meganmcgeee <meganpmcgee@gmail.com>
2016-05-04 11:54:29 -0700

    Added a controller so Simple bot says hello and asks for users name if they have never given a name before. If they haave given a name, SB asks how things are going and addresses user by name

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@ bot.startRTM((err: Error) => {
     throw new Error('Could not connect to Slack');
   }
 });
+
+// SAMPLE CODE FROM SAMPLE slackbot
+
+// Intro
+
+controller.hears(['hello', 'hi', 'yo', 'sup', 'hey'], 'direct_message,direct_mention,mention',
+function(bot, message) {
+  controller.storage.users.get(message.user, (err, user) => {
+    if (user && user.name) {
+      bot.reply(message, "Oh, hey. How's it goin' " + user.name + "?");
+    } else {
+      bot.reply(message, "Hey, I'm SimpleBot, what's your name, g?");
+    }
+  });
+});
